d0f8e1e12f15444f220f47c417d23f0c16d21339
aleksandrgineyka <meinepostkam@gmail.com>
2023-03-30 12:28:12 +0300

    feat: to add configuration classes AppConfig and WebConfig

--- src/main/java/ru/javarush/config/AppConfig.java
+++ src/main/java/ru/javarush/config/AppConfig.java
@@ -1,7 +1,57 @@
 package ru.javarush.config;
 
+import com.zaxxer.hikari.HikariDataSource;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Enumerated;
+import org.hibernate.cfg.Environment;
+import org.springframework.cglib.proxy.Factory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
+@EnableTransactionManagement
 public class AppConfig {
+    @Bean
+    public LocalSessionFactoryBean localSessionFactoryBean() {
+        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSource());
+        sessionFactoryBean.setPackagesToScan("ru.javarush.entity");
+        sessionFactoryBean.setHibernateProperties(properties());
+        return sessionFactoryBean;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/todo");
+        dataSource.setUsername("root");
+        dataSource.setPassword("postgres");
+        dataSource.setMaximumPoolSize(10);
+        return dataSource;
+    }
+
+    @Bean
+    public Properties properties() {
+        Properties properties = new Properties();
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+        properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+        properties.put(Environment.HBM2DDL_AUTO, "validate");
+        return properties;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory factory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(factory);
+        return transactionManager;
+    }
 }
--- src/main/java/ru/javarush/config/WebConfig.java
+++ src/main/java/ru/javarush/config/WebConfig.java
@@ -1,6 +1,55 @@
 package ru.javarush.config;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 
-public class WebConfig {
+@Configuration
+@EnableWebMvc
+@ComponentScan("ru.javarush")
+public class WebConfig implements WebMvcConfigurer {
+    private final ApplicationContext context;
+
+    public WebConfig(ApplicationContext context) {
+        this.context = context;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver springResourceTemplateResolver() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setApplicationContext(context);
+        resolver.setPrefix("/html/");
+        resolver.setSuffix(".html");
+        resolver.setCacheable(false);
+        return resolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine engine() {
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.setTemplateResolver(springResourceTemplateResolver());
+        engine.setEnableSpringELCompiler(true);
+        return engine;
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("tasks");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/html/**").addResourceLocations("/html/");
+        registry.addResourceHandler("/style/**").addResourceLocations("/style/");
+        registry.addResourceHandler("/script/**").addResourceLocations("/script/");
+    }
 }
